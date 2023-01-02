d3982938f5638fe88b8183bf7ecfe7f57ea4d94b
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-02 18:03:08 +0100

    create app

---
+++ src/Componente1.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const Componente1 = () => {
+  return (
+    <React.Fragment>
+        <h1>TITOLO1</h1>
+        <h2> titolo 2</h2>
+    </React.Fragment>
+  )
+}
+
+export default Componente1
