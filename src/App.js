fee925f1d38118690e723e0b42ae4d48a8dceb82
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-03 13:44:56 +0100

    amazon gift card and covid mask props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Card from "./Card";
 
-const firstCard = {
+const amazonGift = {
   nome : "10 euro Amazon",
   prezzo : 10,
   img : "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11900.jpg"
 };
-const secondCard = {
+const covidMask = {
   nome : "Mascherine 50 pz",
   prezzo : 20,
   img : "https://www.dentaltix.com/it/sites/default/files/styles/large/public/ffp2-mascarilla-plegable-de-color-negro.jpeg?itok=GMTlXGrM"
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <main>
-        <Card nome={firstCard.nome} prezzo={firstCard.prezzo} img={firstCard.img}  />
-        <Card nome={secondCard.nome} prezzo={secondCard.prezzo} img={secondCard.img} />
+        <Card nome={amazonGift.nome} prezzo={amazonGift.prezzo} img={amazonGift.img}  />
+        <Card nome={covidMask.nome} prezzo={covidMask.prezzo} img={covidMask.img} />
 
       </main>
   
