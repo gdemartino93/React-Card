55ccbb6199061c88e82125cb2b4598dc1f05306c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-03 13:43:49 +0100

    card built using props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Card from "./Card";
+
+const firstCard = {
+  nome : "10 euro Amazon",
+  prezzo : 10,
+  img : "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11900.jpg"
+};
+const secondCard = {
+  nome : "Mascherine 50 pz",
+  prezzo : 20,
+  img : "https://www.dentaltix.com/it/sites/default/files/styles/large/public/ffp2-mascarilla-plegable-de-color-negro.jpeg?itok=GMTlXGrM"
+}
+
 function App() {
   return (
     <div className="App">
       <main>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card nome={firstCard.nome} prezzo={firstCard.prezzo} img={firstCard.img}  />
+        <Card nome={secondCard.nome} prezzo={secondCard.prezzo} img={secondCard.img} />
+
       </main>
   
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,15 @@
 import React from 'react'
-const productImg = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11900.jpg";
-const descriptionImg = "Amazon gift card";
-const price = 20;
-const Card = () => {
+
+
+const Card = (props) => {
   return (
     <section className='Card'>
-        <img src={productImg} alt={descriptionImg} />
-        <h3>Amazon 35euro Gift Card</h3>
+        <img src={props.img} alt={props.nome} />
+        <h3>{props.nome}</h3>
         <hr></hr>
-        <span className='price'> {price} euro</span>
+        <span className='price'> {props.prezzo} euro</span>
         <span className='soldBy'> Venduto da xxxmaster</span>
-        <button>Compra ora per {price} euro</button>
+        <button>Compra ora per {props.prezzo} euro</button>
     </section>
   )
 }
