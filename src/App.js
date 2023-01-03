b8f446888f0b0908f95bc10c02d417c5912b0b68
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-03 17:27:22 +0100

    new product with spread operator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Card from "./Card";
 
 const amazonGift = {
-  nome : "10 euro Amazon",
+  nome : "Amazon Giftcard",
   prezzo : 10,
   img : "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11900.jpg"
 };
@@ -10,6 +10,16 @@ const covidMask = {
   prezzo : 20,
   img : "https://www.dentaltix.com/it/sites/default/files/styles/large/public/ffp2-mascarilla-plegable-de-color-negro.jpeg?itok=GMTlXGrM"
 }
+const montblanc = {
+  nome : "Penna Montblanc",
+  prezzo : 250,
+  img : "https://www.montblanc.com/variants/images/30828384629622771/A/w2500.jpg"
+}
+const reactCourse = {
+  nome : "Corso React by Gianluca",
+  prezzo : 10000,
+  img : "https://amerlin.keantex.com/wp-content/uploads/2021/02/react.png"
+}
 
 function App() {
   return (
@@ -17,6 +27,8 @@ function App() {
       <main>
         <Card {...amazonGift} />
         <Card {...covidMask}/>
+        <Card {...montblanc}/>
+        <Card {...reactCourse} />
 
       </main>
   
