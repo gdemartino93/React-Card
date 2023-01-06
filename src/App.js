56a4a6e9206bb576fbd9aec782bd404155517c87
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-06 01:52:28 +0100

    iteration obj array

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import Card from "./Card";
+import products from "./Products";
 
-const amazonGift = {
-  nome : "Amazon Giftcard",
-  prezzo : 10,
-  img : "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11900.jpg",
-  soldBy : "Amazon s.p.a."
-};
-const covidMask = {
-  nome : "Mascherine 50 pz",
-  prezzo : 20,
-  img : "https://www.dentaltix.com/it/sites/default/files/styles/large/public/ffp2-mascarilla-plegable-de-color-negro.jpeg?itok=GMTlXGrM",
-  soldBy : "Sanitary Services s.p.a"
-}
-const montblanc = {
-  nome : "Penna Montblanc",
-  prezzo : 250,
-  img : "https://www.montblanc.com/variants/images/30828384629622771/A/w2500.jpg",
-  soldBy : "Montblanc s.p.a"
-}
-const reactCourse = {
-  nome : "Corso React",
-  prezzo : 10000,
-  img : "https://amerlin.keantex.com/wp-content/uploads/2021/02/react.png",
-  soldBy : "Gianluca"
-}
-const tshirtMilan = {
-  nome : "Maglia Milan",
-  prezzo : 100,
-  img : "https://www.footballitalia.it/69635-large_default/ac-milan-maglia-gara-home-puma-21-22-bambino-da-5a-16-anni.jpg",
-  soldBy : "Ac Milan"
-}
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Card {...amazonGift}>
-        <div className="offerta">
-          <span> OFFERTA </span>
+        <div className="container">
+          {products.map(product =>{
+            return <Card key={product.id} {...product}/>
+          })}
         </div>
-        </Card>
-        <Card {...covidMask}>
-         
-        </Card>
-        <Card {...montblanc}/>
-        <Card {...reactCourse} />
-        <Card {...tshirtMilan} />
-
       </main>
-  
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const Card = ({nome, prezzo, img, soldBy,children} ) => {
+const Card = ({nome, prezzo, img, soldBy,prezzoAttuale,children,limited} ) => {
+
+  const showPrice = () =>{
+    console.log(prezzo)
+  };
+
   return (
+    
     <section className='Card'>
         <div>{children}</div>
         <img src={img} alt={nome} />
@@ -9,7 +15,12 @@ const Card = ({nome, prezzo, img, soldBy,children} ) => {
         <hr></hr>
         <span className='price'> {prezzo} euro</span>
         <span className='soldBy'> Venduto da {soldBy}</span>
-        <button>Compra ora per {prezzo} euro</button>
+        <button onClick={()=> alert(`nome: ${nome}`)} >Compra ora per {prezzo} euro</button>
+        <button onClick={()=> showPrice()} >Controlla il prezzo</button>
+        <span>Prezzo attuale:</span>
+        <span>{prezzoAttuale}</span>
+        <p>{limited}</p>
+   
     </section>
   )
 }
